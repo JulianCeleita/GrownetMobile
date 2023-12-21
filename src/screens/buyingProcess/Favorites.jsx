8088faa3c1809cb2fe94ae67038163feb6f52fb1
pvgmoon <pandreavanegasg@gmail.com>
2023-12-21 15:34:28 -0500

    Inicio vista favoritos

--- src/screens/buyingProcess/Favorites.jsx
+++ src/screens/buyingProcess/Favorites.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { Text, TouchableOpacity, View } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { FavoritesStyle } from '../../styles/FavoritesStyle'
+import { GlobalStyles } from '../../styles/Styles'
 
 const Favorites = () => {
   return (
-    <View>
-      <Text>Favorites</Text>
-    </View>
+    <SafeAreaView style={FavoritesStyle.favorites}>
+      <View>
+        <Text>Tus favoritos</Text>
+      </View>
+      <View style={[FavoritesStyle.card, GlobalStyles.boxShadow]}>
+        <Text style={FavoritesStyle.tittle}>Todavía no tienes favoritos</Text>
+        <Text style={FavoritesStyle.text}>
+          Navega por productos y escoge tus favoritos seleccionando el icono del
+          corazón
+        </Text>
+        <TouchableOpacity style={[GlobalStyles.btnPrimary, { width: 200 }]}>
+          <Text style={GlobalStyles.textBtnSecundary}>Ir a productos</Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
   )
 }
 
---
+++ src/styles/FavoritesStyle.jsx
@@ -0,0 +1,28 @@
+import { StyleSheet } from 'react-native'
+
+export const FavoritesStyle = StyleSheet.create({
+  favorites: {
+    backgroundColor: 'white',
+    height: '100%',
+  },
+  card: {
+    backgroundColor: 'white',
+    padding: 20,
+    margin: 25,
+    borderRadius: 15,
+    alignItems: 'center',
+  },
+  text: {
+    fontFamily: 'PoppinsRegular',
+    fontSize: 15,
+    color: '#04444F',
+    textAlign: 'center',
+    marginBottom: 10,
+  },
+  tittle: {
+    fontFamily: 'PoppinsSemi',
+    fontSize: 18,
+    marginTop: 20,
+    textAlign: 'center',
+  },
+})
