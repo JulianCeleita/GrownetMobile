2520a98a7e786ce1f9fb53ba4e19b63e688f676a
Julian Celeita <andresceleita@yahoo.es>
2023-12-23 01:32:42 -0500

    Reparado aviso de que no hay ordenes

--- src/screens/Record/Records.jsx
+++ src/screens/Record/Records.jsx
@@ -74,148 +74,144 @@ const Records = () => {
   return (
     <SafeAreaView style={RecordStyle.record}>
       <ScrollView>
-        {/* AVISO DE QUE NO HAY ORDENES */}
-        {allOrders.length === 0 ? (
-          <View style={RecordStyle.recordZero}>
-            <Image source={require('../../../assets/img/img-succesful.png')} />
-            <Text style={RecordStyle.textZero}>{t('record.noOrders')}</Text>
-            <TouchableOpacity
-              style={GlobalStyles.btnPrimary}
-              onPress={() => navigation.navigate('suppliers')}
+        <View style={[RecordStyle.tabContainer, GlobalStyles.boxShadow]}>
+          {/* SWITCH ENTRE SETTINGS Y ORDERS */}
+          <TouchableOpacity
+            style={[
+              {
+                flex: 1,
+                backgroundColor:
+                  activeTab === 'allOrders' ? '#62c471' : 'white',
+                padding: 10,
+                alignItems: 'center',
+              },
+              RecordStyle.btnTab,
+            ]}
+            onPress={switchTab}
+          >
+            <Text
+              style={{
+                marginTop: 5,
+                fontFamily: 'PoppinsRegular',
+                color: activeTab === 'allOrders' ? 'white' : '#04444f',
+              }}
             >
-              <Text style={GlobalStyles.textBtnSecundary}>
-                {t('record.bttnNoOrders')}
-              </Text>
-            </TouchableOpacity>
-          </View>
-        ) : (
-          <>
-            <View style={[RecordStyle.tabContainer, GlobalStyles.boxShadow]}>
-              {/* SWITCH ENTRE SETTINGS Y ORDERS */}
-              <TouchableOpacity
-                style={[
-                  {
-                    flex: 1,
-                    backgroundColor:
-                      activeTab === 'allOrders' ? '#62c471' : 'white',
-                    padding: 10,
-                    alignItems: 'center',
-                  },
-                  RecordStyle.btnTab,
-                ]}
-                onPress={switchTab}
-              >
-                <Text
-                  style={{
-                    marginTop: 5,
-                    fontFamily: 'PoppinsRegular',
-                    color: activeTab === 'allOrders' ? 'white' : '#04444f',
-                  }}
-                >
-                  {t('record.allOrders')}
-                </Text>
-              </TouchableOpacity>
-              <TouchableOpacity
-                style={[
-                  {
-                    flex: 1,
-                    backgroundColor:
-                      activeTab === 'settings' ? '#62c471' : 'white',
-                    padding: 10,
-                    alignItems: 'center',
-                  },
-                  RecordStyle.btnTab,
-                ]}
-                onPress={() => setActiveTab('settings')}
-              >
-                <Text
-                  style={{
-                    marginTop: 5,
-                    fontFamily: 'PoppinsRegular',
-                    color: activeTab === 'settings' ? 'white' : '#04444f',
-                  }}
-                >
-                  {t('record.settings')}
-                </Text>
-              </TouchableOpacity>
-            </View>
+              {t('record.allOrders')}
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={[
+              {
+                flex: 1,
+                backgroundColor: activeTab === 'settings' ? '#62c471' : 'white',
+                padding: 10,
+                alignItems: 'center',
+              },
+              RecordStyle.btnTab,
+            ]}
+            onPress={() => setActiveTab('settings')}
+          >
+            <Text
+              style={{
+                marginTop: 5,
+                fontFamily: 'PoppinsRegular',
+                color: activeTab === 'settings' ? 'white' : '#04444f',
+              }}
+            >
+              {t('record.settings')}
+            </Text>
+          </TouchableOpacity>
+        </View>
+        <View>
+          {/* VISTA SI ESTÁ SETTINGS SELECCIONADO */}
+          {activeTab === 'settings' ? (
+            <Settings />
+          ) : (
             <View>
-              {/* VISTA SI ESTÁ SETTINGS SELECCIONADO */}
-              {activeTab === 'settings' ? (
-                <Settings />
+              {/* VISTA SI ESTÁ ORDERS SELECCIONADO */}
+                {/* AVISO DE QUE NO HAY ORDENES */}
+              {allOrders.length === 0 ? (
+                <View style={RecordStyle.recordZero}>
+                  <Image
+                    source={require('../../../assets/img/img-succesful.png')}
+                  />
+                  <Text style={RecordStyle.textZero}>
+                    {t('record.noOrders')}
+                  </Text>
+                  <TouchableOpacity
+                    style={GlobalStyles.btnPrimary}
+                    onPress={() => navigation.navigate('suppliers')}
+                  >
+                    <Text style={GlobalStyles.textBtnSecundary}>
+                      {t('record.bttnNoOrders')}
+                    </Text>
+                  </TouchableOpacity>
+                </View>
               ) : (
                 <View>
-                  {/* VISTA SI ESTÁ ORDERS SELECCIONADO */}
-                  <View>
-                    {allOrders.map((order) => (
-                      <TouchableOpacity
-                        style={[RecordStyle.cardRecord, GlobalStyles.boxShadow]}
-                        key={order.reference}
-                        onPress={() => handleOrderSelected(order)}
-                      >
-                        <View
-                          style={RecordStyle.textCard}
-                          key={order.reference}
-                        >
-                          {order.id_stateOrders !== 5 &&
-                            order.id_stateOrders !== 6 && (
-                              <Text style={RecordStyle.btnPending}>
-                                {t('record.pending')}
-                              </Text>
-                            )}
-                          {order.id_stateOrders === 5 && (
-                            <Text style={RecordStyle.btnDelivered}>
-                              {t('record.delivered')}
+                  {allOrders.map((order) => (
+                    <TouchableOpacity
+                      style={[RecordStyle.cardRecord, GlobalStyles.boxShadow]}
+                      key={order.reference}
+                      onPress={() => handleOrderSelected(order)}
+                    >
+                      <View style={RecordStyle.textCard} key={order.reference}>
+                        {order.id_stateOrders !== 5 &&
+                          order.id_stateOrders !== 6 && (
+                            <Text style={RecordStyle.btnPending}>
+                              {t('record.pending')}
                             </Text>
                           )}
-                          {order.id_stateOrders === 6 && (
-                            <Text style={RecordStyle.btnDispute}>
-                              {t('record.dispute')}
-                            </Text>
+                        {order.id_stateOrders === 5 && (
+                          <Text style={RecordStyle.btnDelivered}>
+                            {t('record.delivered')}
+                          </Text>
+                        )}
+                        {order.id_stateOrders === 6 && (
+                          <Text style={RecordStyle.btnDispute}>
+                            {t('record.dispute')}
+                          </Text>
+                        )}
+                        <Text style={RecordStyle.tittle}>
+                          {t('record.order')}
+                        </Text>
+                        <Text style={RecordStyle.text}>{order.reference}</Text>
+                      </View>
+                      <View style={RecordStyle.textCard}>
+                        <Text style={RecordStyle.btnSpace}></Text>
+                        <Text style={RecordStyle.tittle}>
+                          {t('record.date')}
+                        </Text>
+                        <Text style={RecordStyle.text}>
+                          {new Date(order.date_delivery).toLocaleDateString(
+                            'en-GB',
+                            {
+                              day: '2-digit',
+                              month: '2-digit',
+                              year: '2-digit',
+                            },
                           )}
-                          <Text style={RecordStyle.tittle}>
-                            {t('record.order')}
-                          </Text>
-                          <Text style={RecordStyle.text}>
-                            {order.reference}
-                          </Text>
-                        </View>
-                        <View style={RecordStyle.textCard}>
-                          <Text style={RecordStyle.btnSpace}></Text>
-                          <Text style={RecordStyle.tittle}>
-                            {t('record.date')}
-                          </Text>
-                          <Text style={RecordStyle.text}>
-                            {new Date(order.date_delivery).toLocaleDateString(
-                              'en-GB',
-                              {
-                                day: '2-digit',
-                                month: '2-digit',
-                                year: '2-digit',
-                              },
-                            )}
-                          </Text>
-                        </View>
-                        <View style={RecordStyle.textCard}>
-                          <Ionicons
-                            name="chevron-forward"
-                            size={24}
-                            color="#62C471"
-                            style={{ margin: 4 }}
-                          />
-                          <Text style={RecordStyle.tittle}>
-                            {t('record.amount')}
-                          </Text>
-                          <Text style={RecordStyle.text}>£{order.total}</Text>
-                        </View>
-                      </TouchableOpacity>
-                    ))}
-                  </View>
+                        </Text>
+                      </View>
+                      <View style={RecordStyle.textCard}>
+                        <Ionicons
+                          name="chevron-forward"
+                          size={24}
+                          color="#62C471"
+                          style={{ margin: 4 }}
+                        />
+                        <Text style={RecordStyle.tittle}>
+                          {t('record.amount')}
+                        </Text>
+                        <Text style={RecordStyle.text}>£{order.total}</Text>
+                      </View>
+                    </TouchableOpacity>
+                  ))}
                 </View>
               )}
             </View>
-          </>
-        )}
+          )}
+        </View>
       </ScrollView>
     </SafeAreaView>
   )
